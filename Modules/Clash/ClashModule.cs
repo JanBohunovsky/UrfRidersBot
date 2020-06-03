a765bd7ee0fe5d706373b36c50be872d1b4ba812
Jan Bohunovský <TheBohush@gmail.com>
2020-06-03 19:33:04 +0200

    Add basic tournament fetching

--- Modules/Clash/ClashModule.cs
+++ Modules/Clash/ClashModule.cs
@@ -1,4 +1,6 @@
-﻿using Discord;
+﻿using System.Text;
+using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
 using Microsoft.Extensions.Logging;
 
@@ -15,6 +17,33 @@ namespace UrfRiders.Modules.Clash
 
         private EmbedBuilder BaseEmbed => new EmbedBuilder().WithColor(Color.Blue);
 
+        [Command("tournaments")]
+        [Alias("upcoming")]
+        public async Task GetTournaments()
+        {
+            if (Settings.ClashChannel != Context.Channel.Id)
+                return;
 
+            var embedBuilder = BaseEmbed.WithTitle("Upcoming Tournaments");
+
+            var tournaments = await Service.GetTournaments();
+            if (tournaments.Length == 0)
+            {
+                await ReplyAsync(embed: embedBuilder.WithDescription("No upcoming tournaments.").Build());
+                return;
+            }
+
+            var description = new StringBuilder();
+            foreach (var tournament in tournaments)
+            {
+                description.AppendLine($"**{tournament.FormattedName}**");
+                description.AppendLine(tournament.FormattedTime);
+                description.AppendLine();
+            }
+
+            embedBuilder.WithDescription(description.ToString());
+            embedBuilder.WithThumbnailUrl("https://cdn.discordapp.com/attachments/717788228899307551/717788431006302218/Clash_Crest_icon.webp");
+            await ReplyAsync(embed: embedBuilder.Build());
+        }
     }
 }
--- Modules/Clash/ClashService.cs
+++ Modules/Clash/ClashService.cs
@@ -1,8 +1,11 @@
-﻿using Discord.WebSocket;
+﻿using System;
+using System.Linq;
+using Discord.WebSocket;
 using LiteDB;
 using Microsoft.Extensions.Logging;
 using MingweiSamuel.Camille;
 using System.Threading.Tasks;
+using MingweiSamuel.Camille.Enums;
 
 namespace UrfRiders.Modules.Clash
 {
@@ -13,17 +16,60 @@ namespace UrfRiders.Modules.Clash
         private readonly ILogger _logger;
         private readonly RiotApi _riotApi;
 
+        private bool _init;
+        private ClashTournamentData[] _tournaments;
+
         public ClashService(DiscordSocketClient client, LiteDatabase database, ILogger<ClashService> logger, RiotApi riotApi)
         {
             _client = client;
             _database = database;
             _logger = logger;
             _riotApi = riotApi;
+
+            // Run periodic update when the client is ready
+            _client.Ready += () =>
+            {
+                if (!_init)
+                {
+                    Task.Run(PeriodicUpdate);
+                    _init = true;
+                }
+                return Task.CompletedTask;
+            };
         }
 
-        public async Task GetTournaments()
+        public async Task<ClashTournamentData[]> GetTournaments()
         {
+            return _tournaments ??= await DownloadTournaments();
+        }
 
+        private async Task<ClashTournamentData[]> DownloadTournaments()
+        {
+            var data = await _riotApi.ClashV1.GetTournamentsAsync(Region.EUNE);
+            return data.Select(ClashTournamentData.Parse).OrderBy(d => d.RegistrationTime).ToArray();
+        }
+
+        private async Task PeriodicUpdate()
+        {
+            while (true)
+            {
+                var target = DateTimeOffset.Now.AddHours(1);
+
+                // Update
+                try
+                {
+                    _tournaments = await DownloadTournaments();
+                }
+                catch (Exception e)
+                {
+                    _logger.LogWarning(e, "Could not fetch upcoming tournaments.");
+                }
+
+                //TODO: Check if each server has seen these tournaments by comparing TournamentIds
+
+                // Wait
+                await Task.Delay(target - DateTimeOffset.Now);
+            }
         }
     }
 }
