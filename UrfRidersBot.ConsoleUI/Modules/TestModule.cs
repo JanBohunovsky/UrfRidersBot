80c640252a35b7919a8c51d24867dbabb15ca2cc
Jan Bohunovský <TheBohush@gmail.com>
2020-12-22 12:58:32 +0100

    Add test module and guild rank definition
    - Test module will contain anything that has to be tested, like new features.
    - Guild rank is simple permission system.
    
    Guild rank system:
    Guilds usually have some typical roles like "member" and "moderator". This system takes advantage of that and offers simpler permission model that uses those roles.
    If a guild doesn't have certain role, there's a fallback check.
    The role checking will be implemented once we'll implement a guild settings system.

---
+++ UrfRidersBot.ConsoleUI/Modules/TestModule.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Discord.Commands;
+using UrfRidersBot.Library;
+using UrfRidersBot.Library.Preconditions;
+
+namespace UrfRidersBot.ConsoleUI.Modules
+{
+    [Group("test")]
+    public class TestModule : BaseModule
+    {
+        [Command("exception")]
+        public Task Exception()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Command("success")]
+        public async Task<RuntimeResult> Success()
+        {
+            return CommandResult.FromSuccess("Yay, everything went well!");
+        }
+
+        [Command("error")]
+        public async Task<RuntimeResult> Error()
+        {
+            return CommandResult.FromError("Something went wrong.");
+        }
+
+        [Command("info")]
+        public async Task<RuntimeResult> Information()
+        {
+            return CommandResult.FromInformation(null, "Hello world!");
+        }
+
+        [Command("everyone")]
+        [RequireGuildRank(GuildRank.Everyone)]
+        public async Task<RuntimeResult> Everyone()
+        {
+            return CommandResult.FromSuccess("Yay, everyone can use this command!");
+        }
+
+        [Command("member")]
+        [RequireGuildRank(GuildRank.Member)]
+        public async Task<RuntimeResult> Member()
+        {
+            return CommandResult.FromSuccess("Yay, you're a verified user on this server!");
+        }
+
+        [Command("moderator")]
+        [RequireGuildRank(GuildRank.Moderator)]
+        public async Task<RuntimeResult> Moderator()
+        {
+            return CommandResult.FromSuccess("You're a moderator! Congrats!");
+        }
+
+        [Command("admin")]
+        [RequireGuildRank(GuildRank.Admin)]
+        public async Task<RuntimeResult> Admin()
+        {
+            return CommandResult.FromSuccess("Damn look at this admin right here!");
+        }
+
+        [Command("owner")]
+        [RequireGuildRank(GuildRank.Owner)]
+        public async Task<RuntimeResult> Owner()
+        {
+            return CommandResult.FromSuccess("The king of the castle! eh.. I mean the owner of this server!");
+        }
+    }
+}
---
+++ UrfRidersBot.Library/Preconditions/RequireGuildRank.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace UrfRidersBot.Library.Preconditions
+{
+    /// <summary>
+    /// Requires the user invoking the command to have this guild rank or higher.
+    /// <para>
+    /// Example: If you select <see cref="GuildRank.Member"/> then users that are at least that rank
+    /// or higher (e.g. <see cref="GuildRank.Moderator"/>) can invoke this command.
+    /// </para>
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = false)]
+    public class RequireGuildRank : PreconditionAttribute
+    {
+        private static readonly Task<PreconditionResult> NotGuild =
+            Task.FromResult(PreconditionResult.FromError("You must be in a guild to run this command."));
+
+        private static readonly Task<PreconditionResult> NotOwner =
+            Task.FromResult(PreconditionResult.FromError("Only the guild owner is permitted to run this command."));
+        
+        private static Task<PreconditionResult> NotPermitted(string missingRank) =>
+            Task.FromResult(PreconditionResult.FromError($"You must be {missingRank} to run this command."));
+
+        private static readonly Task<PreconditionResult> Permitted =
+            Task.FromResult(PreconditionResult.FromSuccess());
+        
+        private readonly GuildRank _rank;
+        
+        public RequireGuildRank(GuildRank rank) => _rank = rank;
+
+        public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider provider)
+        {
+            // Only valid in guild.
+            if (!(context.User is SocketGuildUser user))
+                return NotGuild;
+
+            // TODO: Get guild settings
+            var guildSettings = new object();
+
+            // Check user's rank
+            var userRank = GetUserRank(user, context.Guild, guildSettings);
+            if (userRank >= _rank)
+                return Permitted;
+            
+            // Not permitted - find out the best error message.
+            if (_rank == GuildRank.Owner)
+            {
+                return NotOwner;
+            }
+            else
+            {
+                var missingRank = GetRankName(_rank, context.Guild, guildSettings);
+                return NotPermitted(missingRank);
+            }
+        }
+
+        private static GuildRank GetUserRank(SocketGuildUser user, IGuild guild, object guildSettings)
+        {
+            var rank = GuildRank.Everyone;
+            if (UserHasRole(user, null) ?? true)
+                rank = GuildRank.Member;
+            if (UserHasRole(user, null) ?? user.GuildPermissions.Has(GuildPermission.ManageChannels))
+                rank = GuildRank.Moderator;
+            if (UserHasRole(user, null) ?? false)
+                rank = GuildRank.Admin;
+            if (guild.OwnerId == user.Id)
+                rank = GuildRank.Owner;
+
+            return rank;
+        }
+
+        private static string GetRankName(GuildRank rank, IGuild guild, object guildSettings)
+        {
+            return rank switch
+            {
+                GuildRank.Everyone   => "everyone",
+                GuildRank.Member     => GetRoleMention(guild, null) ?? "a member",
+                GuildRank.Moderator  => GetRoleMention(guild, null) ?? "a moderator",
+                GuildRank.Admin      => GetRoleMention(guild, null) ?? "an admin",
+                GuildRank.Owner      => "the guild owner",
+                _                    => throw new ArgumentOutOfRangeException(nameof(rank))
+            };
+        }
+
+        /// <summary>
+        /// Checks if user has a role. If <see cref="roleId"/> is null then this method also returns null.
+        /// <para>
+        /// This is a little helper method so I don't have to check if <see cref="roleId"/> is null, or I don't have to use it twice.
+        /// With this I can use the null coalescing operator (the double question mark).
+        /// </para>
+        /// </summary>
+        private static bool? UserHasRole(SocketGuildUser user, ulong? roleId)
+        {
+            if (roleId == null)
+                return null;
+            
+            return user.Roles.Any(r => r.Id == roleId);
+        }
+
+        private static string? GetRoleMention(IGuild guild, ulong? roleId)
+        {
+            if (roleId == null)
+                return null;
+            
+            return guild.GetRole(roleId.Value)?.Mention;
+        }
+    }
+}
