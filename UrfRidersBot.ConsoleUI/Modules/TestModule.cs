41f6f35f3d69dca8fa6995f1459ed48b048cd322
Jan Bohunovský <TheBohush@gmail.com>
2020-12-23 11:44:27 +0100

    Initial database access test

--- UrfRidersBot.ConsoleUI/Modules/TestModule.cs
+++ UrfRidersBot.ConsoleUI/Modules/TestModule.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
+using Raven.Client.Documents;
 using UrfRidersBot.Library;
 using UrfRidersBot.Library.Preconditions;
 
@@ -10,6 +12,8 @@ namespace UrfRidersBot.ConsoleUI.Modules
     [Group("test")]
     public class TestModule : BaseModule
     {
+        public IDocumentStore Store { get; set; } = null!;
+        
         [Command("exception")]
         public Task Exception()
         {
@@ -67,6 +71,29 @@ namespace UrfRidersBot.ConsoleUI.Modules
         public async Task<RuntimeResult> Owner()
         {
             return CommandResult.FromSuccess("The king of the castle! eh.. I mean the owner of this server!");
+        }
+
+        [Command("database")]
+        [Alias("db")]
+        public async Task Database(string? value = null)
+        {
+            using var db = Store.OpenAsyncSession();
+            var guildData = await db.LoadAsync<GuildData>(GuildData.GetId(Context.Guild));
+            // var guildData = await db.Query<GuildData>().FirstOrDefaultAsync(x => x.GuildId == Context.Guild.Id);
+            if (guildData == null)
+            {
+                guildData = GuildData.FromGuild(Context.Guild);
+                await db.StoreAsync(guildData);
+            }
+
+            var embed = Embed.Basic(title: "Database test")
+                .AddField("Before", guildData.RandomValue ?? "`null`", true)
+                .AddField("After", value ?? "`null`", true);
+
+            guildData.RandomValue = value;
+            await db.SaveChangesAsync();
+
+            await ReplyAsync(embed: embed.Build());
         }
     }
 }
---
+++ UrfRidersBot.Library/Models/GuildData.cs
@@ -0,0 +1,19 @@
+﻿using Discord;
+
+namespace UrfRidersBot.Library
+{
+    public class GuildData
+    {
+        public string Id => GetId(GuildId);
+        public ulong GuildId { get; internal set; }
+        public string? RandomValue { get; set; }
+        
+        private GuildData() {}
+
+        public static GuildData FromGuild(IGuild guild) => new GuildData { GuildId = guild.Id };
+
+        public static string GetId(ulong guildId)=> $"GuildData/{guildId}";
+        
+        public static string GetId(IGuild guild) => GetId(guild.Id);
+    }
+}
