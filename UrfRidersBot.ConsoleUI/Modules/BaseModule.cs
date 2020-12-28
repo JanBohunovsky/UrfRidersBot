7040d7019aac3d29df7d2bd8baff884f85e8f923
Jan Bohunovský <TheBohush@gmail.com>
2020-12-28 10:47:42 +0100

    Add custom command context
    Currently it's not anything special but it will be useful later.

--- UrfRidersBot.ConsoleUI/Modules/BaseModule.cs
+++ UrfRidersBot.ConsoleUI/Modules/BaseModule.cs
@@ -3,7 +3,7 @@ using UrfRidersBot.Library;
 
 namespace UrfRidersBot.ConsoleUI.Modules
 {
-    public class BaseModule : ModuleBase<SocketCommandContext>
+    public class BaseModule : ModuleBase<UrfRidersContext>
     {
         public IEmbedService Embed { get; set; } = null!;
     }
---
+++ UrfRidersBot.Library/Models/UrfRidersContext.cs
@@ -0,0 +1,14 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace UrfRidersBot.Library
+{
+    public class UrfRidersContext : SocketCommandContext
+    {
+        public UrfRidersContext(DiscordSocketClient client, SocketUserMessage msg) : base(client, msg)
+        {
+            
+        }
+    }
+}
