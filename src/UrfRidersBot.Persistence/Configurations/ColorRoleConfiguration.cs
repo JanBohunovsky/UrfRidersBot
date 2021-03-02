e9df45272f12205ffbce3948389475b719cd5644
Jan Bohunovský <TheBohush@gmail.com>
2021-03-02 16:31:03 +0100

    Add ColorRoles table

---
+++ src/UrfRidersBot.Persistence/Configurations/ColorRoleConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UrfRidersBot.Persistence.DTOs;
+
+namespace UrfRidersBot.Persistence.Configurations
+{
+    internal class ColorRoleConfiguration : IEntityTypeConfiguration<ColorRoleDTO>
+    {
+        public void Configure(EntityTypeBuilder<ColorRoleDTO> builder)
+        {
+            builder.HasKey(x => new { x.GuildId, x.RoleId, x.UserId });
+
+            builder.HasIndex(x => new { x.GuildId, x.RoleId }).IsUnique();
+            builder.HasIndex(x => new { x.GuildId, x.UserId }).IsUnique();
+        }
+    }
+}
