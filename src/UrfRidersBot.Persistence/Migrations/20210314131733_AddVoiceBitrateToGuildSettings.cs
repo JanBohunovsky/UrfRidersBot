b5ecaa09bd8a83f64acb3a462b9b6809ec73b100
Jan Bohunovský <TheBohush@gmail.com>
2021-03-14 14:45:32 +0100

    Add bitrate settings for auto voice channels

---
+++ src/UrfRidersBot.Persistence/Migrations/20210314131733_AddVoiceBitrateToGuildSettings.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UrfRidersBot.Persistence.Migrations
+{
+    public partial class AddVoiceBitrateToGuildSettings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "VoiceBitrate",
+                table: "GuildSettings",
+                type: "integer",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VoiceBitrate",
+                table: "GuildSettings");
+        }
+    }
+}
