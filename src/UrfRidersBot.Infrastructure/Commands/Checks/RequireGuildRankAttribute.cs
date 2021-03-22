f41f59b54d93841fc0a489579667530ecbe200b0
Jan Bohunovský <TheBohush@gmail.com>
2021-03-22 16:26:54 +0100

    Add command checks

---
+++ src/UrfRidersBot.Infrastructure/Commands/Checks/RequireGuildRankAttribute.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.Entities;
+using Microsoft.Extensions.DependencyInjection;
+using Qmmands;
+using UrfRidersBot.Core.Entities;
+using UrfRidersBot.Core.Interfaces;
+
+namespace UrfRidersBot.Infrastructure.Commands.Checks
+{
+    public class RequireGuildRankAttribute : CheckAttribute
+    {
+        private readonly GuildRank _rank;
+        
+        public RequireGuildRankAttribute(GuildRank rank) => _rank = rank;
+        
+        public override async ValueTask<CheckResult> CheckAsync(CommandContext _)
+        {
+            var context = (UrfRidersCommandContext)_;
+            
+            // Get guild settings
+            var unitOfWorkFactory = context.ServiceProvider.GetRequiredService<IUnitOfWorkFactory>();
+            await using var unitOfWork = unitOfWorkFactory.Create();
+            var guildSettings = await unitOfWork.GuildSettings.GetOrCreateAsync(context.Guild);
+            
+            // Check member's rank
+            var memberRank = GetMemberRank(context.Member, context.Channel, guildSettings);
+            return memberRank >= _rank
+                ? CheckResult.Successful
+                : CheckResult.Unsuccessful($"You must be at least {_rank.ToString()} to execute this command.");
+        }
+
+        /// <summary>
+        /// Figures out member's highest guild rank.
+        /// </summary>
+        private static GuildRank GetMemberRank(DiscordMember member, DiscordChannel channel, GuildSettings guildSettings)
+        {
+            var rank = GuildRank.Everyone;
+            if (MemberHasRole(member, guildSettings.MemberRoleId) ?? true)
+                rank = GuildRank.Member;
+            if (MemberHasRole(member, guildSettings.ModeratorRoleId) ??
+                member.PermissionsIn(channel).HasPermission(Permissions.ManageChannels))
+                rank = GuildRank.Moderator;
+            if (MemberHasRole(member, guildSettings.AdminRoleId) ??
+                member.PermissionsIn(channel).HasPermission(Permissions.Administrator))
+                rank = GuildRank.Admin;
+            if (member.Guild.OwnerId == member.Id)
+                rank = GuildRank.Owner;
+
+            return rank;
+        }
+        
+        /// <summary>
+        /// Checks if user has a role. If <see cref="roleId"/> is null then this method also returns null.
+        /// <para>
+        /// This is a little helper method so I don't have to check if <see cref="roleId"/> is null, or I don't have to use it twice.
+        /// With this I can use the null coalescing operator (the double question mark).
+        /// </para>
+        /// </summary>
+        private static bool? MemberHasRole(DiscordMember member, ulong? roleId)
+        {
+            if (roleId == null)
+                return null;
+            
+            return member.Roles.Any(r => r.Id == roleId);
+        }
+    }
+}
---
+++ src/UrfRidersBot.Infrastructure/Commands/Checks/RequireOwnerAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Qmmands;
+
+namespace UrfRidersBot.Infrastructure.Commands.Checks
+{
+    public class RequireOwnerAttribute : CheckAttribute
+    {
+        public override ValueTask<CheckResult> CheckAsync(CommandContext _)
+        {
+            var context = (UrfRidersCommandContext)_;
+
+            var owner = context.Client.CurrentApplication.Owners.First();
+            return owner.Id == context.User.Id
+                ? CheckResult.Successful
+                : CheckResult.Unsuccessful("This can only be executed by the bot's owner.");
+        }
+    }
+}
