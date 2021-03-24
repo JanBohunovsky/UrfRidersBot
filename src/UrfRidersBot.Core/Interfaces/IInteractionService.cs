32ff0289ff72dc8ca808368a80d4a47a74e0ee84
Jan Bohunovský <TheBohush@gmail.com>
2021-03-24 17:39:19 +0100

    Add base entities for slash commands

---
+++ src/UrfRidersBot.Core/Interfaces/IInteractionService.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using UrfRidersBot.Core.Commands.Entities;
+
+namespace UrfRidersBot.Core.Interfaces
+{
+    public interface IInteractionService
+    {
+        Task CreateResponseAsync(
+            ulong interactionId,
+            string token,
+            DiscordInteractionResponseType type, 
+            DiscordInteractionResponseBuilder? builder = null);
+
+        Task EditResponseAsync(string token, DiscordInteractionResponseBuilder builder);
+
+        Task DeleteResponseAsync(string token);
+    }
+}
