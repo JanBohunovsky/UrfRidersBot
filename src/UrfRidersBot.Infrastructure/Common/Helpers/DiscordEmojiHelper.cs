91c4d27b93b9267a7e02cab576e0b9e508ef9ecf
Jan Bohunovský <TheBohush@gmail.com>
2021-04-10 14:59:26 +0200

    Add emoji parser and add guild check to message parser

---
+++ src/UrfRidersBot.Infrastructure/Common/Helpers/DiscordEmojiHelper.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.RegularExpressions;
+using DSharpPlus;
+using DSharpPlus.Entities;
+
+namespace UrfRidersBot.Infrastructure.Common
+{
+    public static class DiscordEmojiHelper
+    {
+        private static readonly Regex GuildEmoteRegex;
+        
+        static DiscordEmojiHelper()
+        {
+            GuildEmoteRegex = new Regex(@"^<a?:[a-zA-Z0-9_]+?:(?<id>\d+)>$", RegexOptions.Compiled);
+        }
+        
+        /// <summary>
+        /// Parses the emoji in either the guild emote format or in unicode. Named emojis are not supported here.
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="emojiText">Raw emoji in either guild emote format (&lt;:name:id&gt;) or in unicode.</param>
+        public static DiscordEmoji? Parse(DiscordClient client, string emojiText)
+        {
+            var match = GuildEmoteRegex.Match(emojiText);
+            if (match.Success)
+            {
+                return ParseGuildEmote(client, match);
+            }
+
+            if (!DiscordEmoji.IsValidUnicode(emojiText))
+            {
+                return null;
+            }
+
+            return DiscordEmoji.FromUnicode(client, emojiText);
+        }
+
+        private static DiscordEmoji? ParseGuildEmote(DiscordClient client, Match match)
+        {
+            if (!ulong.TryParse(match.Groups["id"].Value, out var id))
+            {
+                return null;
+            }
+
+            return DiscordEmoji.FromGuildEmote(client, id);
+        }
+    }
+}
