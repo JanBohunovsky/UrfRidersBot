26c4c018b046a60f3e6d4e1ffa9fb029380853c8
Jan Bohunovský <TheBohush@gmail.com>
2021-04-12 17:05:00 +0200

    Remove custom prefix from guild settings

--- src/UrfRidersBot.Persistence/Migrations/20210412150243_RemoveCustomPrefixFromGuildSettings.Designer.cs
+++ src/UrfRidersBot.Persistence/Migrations/20210412150243_RemoveCustomPrefixFromGuildSettings.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using UrfRidersBot.Persistence.Common;
@@ -9,9 +10,10 @@ using UrfRidersBot.Persistence.Common;
 namespace UrfRidersBot.Persistence.Migrations
 {
     [DbContext(typeof(UrfRidersDbContext))]
-    partial class UrfRidersDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210412150243_RemoveCustomPrefixFromGuildSettings")]
+    partial class RemoveCustomPrefixFromGuildSettings
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
