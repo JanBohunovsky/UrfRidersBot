7f3b71ae285d89ffdc21c4aaae5d92c3078c6166
Jan Bohunovský <TheBohush@gmail.com>
2021-04-12 17:17:55 +0200

    Remove command prefix

--- src/UrfRidersBot.Core/Common/Configuration/DiscordOptions.cs
+++ src/UrfRidersBot.Core/Common/Configuration/DiscordOptions.cs
@@ -4,7 +4,6 @@
     {
         public const string SectionName = "Discord";
         
-        public string Prefix { get; set; } = "!";
         public string? Token { get; set; }
         public string? ClientId { get; set; }
         public string? ClientSecret { get; set; }
