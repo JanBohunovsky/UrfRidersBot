0c6336ee3caa3b8f437152ad141e48fa1af83a69
Jan Bohunovský <TheBohush@gmail.com>
2021-04-19 08:28:44 +0200

    Add docstrings to guild settings repository

--- src/UrfRidersBot.Core/Settings/IGuildSettingsRepository.cs
+++ src/UrfRidersBot.Core/Settings/IGuildSettingsRepository.cs
@@ -5,9 +5,29 @@ namespace UrfRidersBot.Core.Settings
 {
     public interface IGuildSettingsRepository : IRepository
     {
+        /// <summary>
+        /// Gets guild settings for configured guild. Returns null if not found.
+        /// </summary>
+        /// <returns></returns>
         ValueTask<GuildSettings?> GetAsync();
+        
+        /// <summary>
+        /// Gets guild settings for configured guild. Returns new instance if not found.
+        /// </summary>
+        /// <returns></returns>
         ValueTask<GuildSettings> GetOrCreateAsync();
+        
+        /// <summary>
+        /// Saves guild settings for configured guild.
+        /// </summary>
+        /// <param name="settings"></param>
+        /// <returns></returns>
         Task SaveAsync(GuildSettings settings);
+        
+        /// <summary>
+        /// Removes guild settings for configured guild.
+        /// </summary>
+        /// <returns></returns>
         Task RemoveAsync();
     }
 }
