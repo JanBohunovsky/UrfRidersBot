daaa6c554665f31a53ef37dd2abc30d2b6432196
Jan Bohunovský <TheBohush@gmail.com>
2021-04-25 12:35:39 +0200

    Fix log level for Serilog

--- src/UrfRidersBot/Program.cs
+++ src/UrfRidersBot/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Events;
 
 namespace UrfRidersBot
 {
@@ -47,7 +48,7 @@ namespace UrfRidersBot
             if (!context.HostingEnvironment.IsProduction()) 
                 return;
 
-            logger.WriteTo.LiteDB("log.db");
+            logger.WriteTo.LiteDB("log.db", restrictedToMinimumLevel: LogEventLevel.Warning);
         }
     }
 }
